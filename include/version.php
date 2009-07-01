9fb28f8ee7567360931ef12da22d1dfd395d6322
oniku_uj <oniku_uj@a7a435a0-a25e-405b-8339-bcd30a48c3eb>
2009-07-01 09:03:54 +0000

    バージョン番号修正
    git-svn-id: http://svn.sourceforge.jp/svnroot/jinrousiki/trunk@35 a7a435a0-a25e-405b-8339-bcd30a48c3eb

--- include/version.php
+++ include/version.php
@@ -1,7 +1,7 @@
 <?php
 //スクリプト群のバージョン
-$script_version = 'Ver. 1.3.0 beta24';
+$script_version = 'Ver. 1.3.0 RC1';
 
 // スクリプト群の最終更新日
-$script_lastupdate = '2009/06/27 (Sat) 02:10';
+$script_lastupdate = '2009/07/01 (Wed) 18:00';
 ?>
