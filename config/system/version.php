89e4d5de37c8a55d027a466155b91b515c6af30d
umethyl <umethyl@a7a435a0-a25e-405b-8339-bcd30a48c3eb>
2012-12-25 17:20:47 +0000

    user_manager_class.php : DB、HTMLクラスを分離
    git-svn-id: http://svn.sourceforge.jp/svnroot/jinrousiki/trunk@694 a7a435a0-a25e-405b-8339-bcd30a48c3eb

--- config/system/version.php
+++ config/system/version.php
@@ -1,13 +1,13 @@
 <?php
 class ScriptInfo {
   //パッケージのバージョン
-  const VERSION = 'Ver. 2.2.0 alpha1';
+  const VERSION = 'Ver. 2.2.0 alpha2';
 
   //パッケージの最終更新日
-  const LAST_UPDATE = '2012/12/24 (Mon) 00:00';
+  const LAST_UPDATE = '2012/12/25 (Tue) 02:05';
 
   //パッケージ化した時の Revision
-  const REVISION = 690;
+  const REVISION = 691;
 
   //パッケージ名
   const PACKAGE = '<a href="http://sourceforge.jp/projects/jinrousiki/">人狼式</a>';
--- include/user_manager_class.php
+++ include/user_manager_class.php
@@ -1,5 +1,5 @@
 <?php
-//-- ユーザ登録処理クラス --//
+//-- ユーザ登録コントローラー --//
 class UserManager {
   //ユーザ登録
   static function Entry() {
@@ -203,21 +203,18 @@ EOF;
 
   //ユーザ登録画面表示
   static function Output() {
-    extract(RQ::ToArray()); //引数を展開
-    if ($user_no > 0) { //登録情報変更モード
-      $query = 'SELECT * FROM user_entry WHERE room_no = %d AND user_no = %d';
-      $stack = DB::FetchAssoc(sprintf($query, $room_no, $user_no), true);
+    if (RQ::$get->user_no > 0) { //登録情報変更モード
+      $stack = UserManagerDB::GetUser();
       if ($stack['session_id'] != Session::GetID()) {
 	HTML::OutputResult('村人登録 [セッションエラー]', 'セッション ID が一致しません');
       }
       foreach ($stack as $key => $value) {
 	if (array_key_exists($key, RQ::$get)) RQ::Set($key, $value);
       }
-      extract(RQ::ToArray()); //引数を再展開
     }
 
-    DB::$ROOM = RoomDataSet::LoadEntryUserPage($room_no);
-    $str = sprintf('%d 番地の村は', $room_no);
+    DB::$ROOM = RoomDataSet::LoadEntryUserPage(RQ::$get->room_no);
+    $str = sprintf('%d 番地の村は', RQ::$get->room_no);
     if (is_null(DB::$ROOM->id)) {
       HTML::OutputResult('村人登録 [村番号エラー]', $str . '存在しません');
     }
@@ -227,190 +224,176 @@ EOF;
     if (DB::$ROOM->status != 'waiting') {
       HTML::OutputResult('村人登録 [入村不可]', $str . 'すでにゲームが開始されています。');
     }
-
     DB::$ROOM->ParseOption(true);
 
+    UserManagerHTML::Output();
+  }
+}
+
+//-- データベースアクセス (UserManager 拡張) --//
+class UserManagerDB {
+  //ユーザ情報取得
+  static function GetUser() {
+    $query = 'SELECT * FROM user_entry WHERE room_no = ? AND user_no = ?';
+    DB::Prepare($query, array(RQ::$get->room_no, RQ::$get->user_no));
+    return DB::FetchAssoc(null, true);
+  }
+}
+
+//-- HTML 生成クラス (UserManager 拡張) --//
+class UserManagerHTML {
+  const PATH = 'img/entry_user';
+  const URL  = 'user_manager.php?room_no=%d';
+  const UNAME_INPUT = '<td><input type="text" id="uname" name="uname" size="30" maxlength="30" value="%s"></td>';
+  const UNAME_EXPLAIN_HEADER = '普段は表示されず、他のユーザ名がわかるのは';
+  const UNAME_EXPLAIN_FOOTER = '死亡したときとゲーム終了後のみです';
+  const UNAME_TRIP = '<br>＃の右側はトリップ専用入力欄です';
+
+  //出力
+  static function Output() {
     HTML::OutputHeader(ServerConfig::TITLE . '[村人登録]', 'entry_user');
     HTML::OutputJavaScript('submit_icon_search');
     HTML::OutputBodyHeader();
+    self::OutputHeader();
+    self::OutputForm();
+    self::OutputWishRole();
+    self::OutputSubmit();
+    self::OutputIcon();
+    Text::Output('</table></div></form>');
+    HTML::OutputFooter();
+  }
 
-    $action_url = sprintf('user_manager.php?room_no=%d', DB::$ROOM->id);
-    if ($user_no > 0) $action_url .= sprintf('&user_no=%d', $user_no);
-    $path           = 'img/entry_user';
-    $room_name      = DB::$ROOM->name;
-    $room_comment   = DB::$ROOM->comment;
-    $room_no        = DB::$ROOM->id;
-    $male_checked   = '';
-    $female_checked = '';
-    switch (RQ::$get->sex) {
-    case 'male':
-      $male_checked   = ' checked';
-      break;
-
-    case 'female':
-      $female_checked = ' checked';
-      break;
-    }
-
-    if ($user_no > 0) {
-      $uname_form = <<<EOF
-<tr>
-<td class="img"><label for="uname"><img src="{$path}/uname.gif" alt="ユーザ名"></label></td>
-<td>{$uname}</td>
-<td class="explain">普段は表示されず、他のユーザ名がわかるのは<br>死亡したときとゲーム終了後のみです</td>
-</tr>
-EOF;
-    }
-    elseif (GameConfig::TRIP) {
-      if (DB::$ROOM->IsOption('necessary_name') && DB::$ROOM->IsOption('necessary_trip')) {
-	$add_message = '<br><span>必ずユーザ名・トリップの両方を入力してください</span>';
-      } elseif (DB::$ROOM->IsOption('necessary_name')) {
-	$add_message = '<br><span>必ずユーザ名を入力してください</span>';
-      } elseif (DB::$ROOM->IsOption('necessary_trip')) {
-	$add_message = '<br><span>必ずトリップを入力してください</span>';
-      } else {
-	$add_message = '';
-      }
-      $uname_form = <<<EOF
-<tr>
-<td class="img"><label for="uname"><img src="{$path}/uname.gif" alt="ユーザ名"></label></td>
-<td><input type="text" id="uname" name="uname" size="30" maxlength="30" value="{$uname}"></td>
-<td><label for="trip">＃</lable> <input type="text" id="trip" name="trip" size="15" maxlength="15" value="{$trip}"></td>
-</tr>
-<tr>
-<td></td>
-<td colspan="2" class="explain">普段は表示されず、他のユーザ名がわかるのは死亡したときとゲーム終了後のみです<br>＃の右側はトリップ専用入力欄です{$add_message}</td>
-</tr>
-EOF;
-    }
-    else {
-      $uname_form = <<<EOF
-<tr>
-<td class="img"><label for="uname"><img src="{$path}/uname.gif" alt="ユーザ名"></label></td>
-<td><input type="text" id="uname" name="uname" size="30" maxlength="30" value="{$uname}"></td>
-<td class="explain">普段は表示されず、他のユーザ名がわかるのは<br>死亡したときとゲーム終了後のみです(<span>トリップ使用不可</span>)</td>
-</tr>
-EOF;
-    }
-
-    if ($user_no < 1) {
-      $password_form = <<<EOF
-<tr>
-<td class="img"><label for="password"><img src="{$path}/password.gif" alt="パスワード"></label></td>
-<td><input type="password" id="password" name="password" size="30" maxlength="30" value=""></td>
-<td class="explain">セッションが切れた場合のログイン時に使います<br> (<span>暗号化されていないので要注意</span>)</td>
-</tr>
-EOF;
-    }
-
-    echo <<<EOF
+  //ヘッダ出力
+  private static function OutputHeader() {
+    $format = <<<EOF
 <a href="./">←戻る</a><br>
-<form method="POST" action="{$action_url}">
+<form method="POST" action="%s">
 <div align="center">
 <table class="main">
-<tr><td><img src="{$path}/title.gif" alt="申請書"></td></tr>
-<tr><td class="title">{$room_name} 村<img src="{$path}/top.gif" alt="への住民登録を申請します"></td></tr>
-<tr><td class="number">～{$room_comment}～ [{$room_no} 番地]</td></tr>
+<tr><td><img src="%s/title.gif" alt="申請書"></td></tr>
+<tr><td class="title">%s 村<img src="%s/top.gif" alt="への住民登録を申請します"></td></tr>
+<tr><td class="number">～%s～ [%d 番地]</td></tr>
+
+EOF;
+    $url = sprintf(self::URL, DB::$ROOM->id);
+    if (RQ::$get->user_no > 0) $url .= sprintf('&user_no=%d', RQ::$get->user_no);
+
+    printf($format,
+	   $url, self::PATH, DB::$ROOM->name, self::PATH, DB::$ROOM->comment, DB::$ROOM->id);
+  }
+
+  //フォーム出力
+  private static function OutputForm() {
+    $format = <<<EOF
 <tr><td>
 <table class="input">
-{$uname_form}
+%s
 <tr>
-<td class="img"><label for="handle_name"><img src="{$path}/handle_name.gif" alt="村人の名前"></label></td>
-<td><input type="text" id="handle_name" name="handle_name" size="30" maxlength="30" value="{$handle_name}"></td>
+<td class="img"><label for="handle_name"><img src="%s/handle_name.gif" alt="村人の名前"></label></td>
+<td><input type="text" id="handle_name" name="handle_name" size="30" maxlength="30" value="%s"></td>
 <td class="explain">村で表示される名前です</td>
 </tr>
-{$password_form}
+%s
 <tr>
-<td class="img"><img src="{$path}/sex.gif" alt="性別"></td>
+<td class="img"><img src="%s/sex.gif" alt="性別"></td>
 <td class="img">
-<label for="male"><img src="{$path}/sex_male.gif" alt="男性"><input type="radio" id="male" name="sex" value="male"{$male_checked}></label>
-<label for="female"><img src="{$path}/sex_female.gif" alt="女性"><input type="radio" id="female" name="sex" value="female"{$female_checked}></label>
+<label for="male"><img src="%s/sex_male.gif" alt="男性"><input type="radio" id="male" name="sex" value="male"%s></label>
+<label for="female"><img src="%s/sex_female.gif" alt="女性"><input type="radio" id="female" name="sex" value="female"%s></label>
 </td>
 <td class="explain">特に意味は無いかも……</td>
 </tr>
 <tr>
-<td class="img"><label for="profile"><img src="{$path}/profile.gif" alt="プロフィール"></label></td>
-<td colspan="2">
-<textarea id="profile" name="profile" cols="30" rows="2">{$profile}</textarea>
-</td>
+<td class="img"><label for="profile"><img src="%s/profile.gif" alt="プロフィール"></label></td>
+<td colspan="2"><textarea id="profile" name="profile" cols="30" rows="2">%s</textarea></td>
 </tr>
 <tr>
 
 EOF;
 
-    if (DB::$ROOM->IsOption('wish_role')){
-      echo <<<EOF
-<td class="role"><img src="{$path}/role.gif" alt="役割希望"></td>
+    $male   = '';
+    $female = '';
+    switch (RQ::$get->sex) {
+    case 'male':
+      $male = ' checked';
+      break;
+
+    case 'female':
+      $female = ' checked';
+      break;
+    }
+
+    printf($format,
+	   self::GenerateUname(), self::PATH, RQ::$get->handle_name, self::GeneratePassword(),
+	   self::PATH, self::PATH, $male, self::PATH, $female, self::PATH, RQ::$get->profile);
+  }
+
+  //希望役職選択フォーム出力
+  private static function OutputWishRole() {
+    if (! DB::$ROOM->IsOption('wish_role')) {
+      echo '<td><input type="hidden" name="role" value="none">';
+      return;
+    }
+
+    $format = <<<EOF
+<td class="role"><img src="%s/role.gif" alt="役割希望"></td>
 <td colspan="2">
 
 EOF;
+    printf($format, self::PATH);
 
-      $wish_role_list = array('none');
-      if (DB::$ROOM->IsChaosWish()) {
-	array_push($wish_role_list,
-		   'human', 'mage', 'necromancer', 'medium', 'priest', 'guard', 'common',
-		   'poison', 'poison_cat', 'pharmacist', 'assassin', 'mind_scanner', 'jealousy',
-		   'brownie', 'wizard', 'doll', 'escaper', 'wolf', 'mad', 'fox', 'child_fox',
-		   'cupid', 'angel', 'quiz', 'vampire', 'chiroptera', 'fairy', 'ogre', 'yaksa',
-		   'duelist', 'avenger', 'patron', 'mania', 'unknown_mania');
-      }
-      elseif (DB::$ROOM->IsOption('gray_random')) {
-	array_push($wish_role_list, 'human', 'wolf', 'mad', 'fox');
-      }
-      else {
-	array_push($wish_role_list,  'human', 'wolf');
-	if (DB::$ROOM->IsQuiz()) {
-	  array_push($wish_role_list, 'mad', 'common', 'fox');
-	}
-	else {
-	  array_push($wish_role_list, 'mage', 'necromancer', 'mad', 'guard', 'common');
-	  if (DB::$ROOM->IsOption('detective')) $wish_role_list[] = 'detective_common';
-	  $wish_role_list[] = 'fox';
-	}
-	if (DB::$ROOM->IsOption('poison'))    $wish_role_list[] = 'poison';
-	if (DB::$ROOM->IsOption('assassin'))  $wish_role_list[] = 'assassin';
-	if (DB::$ROOM->IsOption('boss_wolf')) $wish_role_list[] = 'boss_wolf';
-	if (DB::$ROOM->IsOption('poison_wolf')) {
-	  array_push($wish_role_list, 'poison_wolf', 'pharmacist');
-	}
-	if (DB::$ROOM->IsOption('possessed_wolf')) $wish_role_list[] = 'possessed_wolf';
-	if (DB::$ROOM->IsOption('sirius_wolf'))    $wish_role_list[] = 'sirius_wolf';
-	if (DB::$ROOM->IsOption('child_fox'))      $wish_role_list[] = 'child_fox';
-	if (DB::$ROOM->IsOption('cupid'))          $wish_role_list[] = 'cupid';
-	if (DB::$ROOM->IsOption('medium')) array_push($wish_role_list, 'medium', 'mind_cupid');
-	if (DB::$ROOM->IsOptionGroup('mania') && ! in_array('mania', $wish_role_list)) {
-	  $wish_role_list[] = 'mania';
-	}
-      }
-
-      echo "<table>\n<tr>";
-      $count = 0;
-      foreach ($wish_role_list as $role) {
-	if ($count > 0 && $count % 4 == 0) echo "</tr>\n<tr>"; //4個ごとに改行
-	$count++;
-	$alt = '←' . ($role == 'none' ? '無し' : RoleData::$main_role_list[$role]);
-	$checked = RQ::$get->role == $role ? ' checked' : '';
-	echo <<<EOF
-<td><label for="{$role}"><input type="radio" id="{$role}" name="role" value="{$role}"{$checked}><img src="{$path}/role_{$role}.gif" alt="{$alt}"></label></td>
-EOF;
-      }
-      echo "</tr>\n</table>";
+    $stack = array('none');
+    if (DB::$ROOM->IsChaosWish()) {
+      array_push($stack,
+		 'human', 'mage', 'necromancer', 'medium', 'priest', 'guard', 'common',
+		 'poison', 'poison_cat', 'pharmacist', 'assassin', 'mind_scanner', 'jealousy',
+		 'brownie', 'wizard', 'doll', 'escaper', 'wolf', 'mad', 'fox', 'child_fox',
+		 'cupid', 'angel', 'quiz', 'vampire', 'chiroptera', 'fairy', 'ogre', 'yaksa',
+		 'duelist', 'avenger', 'patron', 'mania', 'unknown_mania');
+    }
+    elseif (DB::$ROOM->IsOption('gray_random')) {
+      array_push($stack, 'human', 'wolf', 'mad', 'fox');
     }
     else {
-      echo '<td><input type="hidden" name="role" value="none">';
+      array_push($stack, 'human', 'wolf');
+      if (DB::$ROOM->IsQuiz()) {
+	array_push($stack, 'mad', 'common', 'fox');
+      }
+      else {
+	array_push($stack, 'mage', 'necromancer', 'mad', 'guard', 'common');
+	if (DB::$ROOM->IsOption('detective')) $stack[] = 'detective_common';
+	$stack[] = 'fox';
+      }
+      foreach (array('poison', 'assassin', 'boss_wolf') as $role) {
+	if (DB::$ROOM->IsOption($role)) $stack[] = $role;
+      }
+      if (DB::$ROOM->IsOption('poison_wolf')) array_push($stack, 'poison_wolf', 'pharmacist');
+      foreach (array('possessed_wolf', 'sirius_wolf', 'child_fox', 'cupid') as $role) {
+	if (DB::$ROOM->IsOption($role)) $stack[] = $role;
+      }
+      if (DB::$ROOM->IsOption('medium')) array_push($stack, 'medium', 'mind_cupid');
+      if (DB::$ROOM->IsOptionGroup('mania') && ! in_array('mania', $stack)) $stack[] = 'mania';
     }
 
-    if (isset(RQ::$get->icon_no) && RQ::$get->icon_no > ($user_no > 0 ? -1 : 0)) {
-      $icon_checked  = ' checked';
-      $input_icon_no = RQ::$get->icon_no;
+    echo "<table>\n<tr>";
+    $format = <<<EOF
+<td><label for="%s"><input type="radio" id="%s" name="role" value="%s"%s><img src="%s/role_%s.gif" alt="%s"></label></td>
+
+EOF;
+    $count = 0;
+    foreach ($stack as $role) {
+      if ($count > 0 && $count % 4 == 0) echo "</tr>\n<tr>"; //4個ごとに改行
+      $count++;
+      $alt = '←' . ($role == 'none' ? '無し' : RoleData::$main_role_list[$role]);
+      $checked = RQ::$get->role == $role ? ' checked' : '';
+      printf($format, $role, $role, $role, $checked, self::PATH, $role, $alt);
     }
-    else{
-      $icon_checked  = '';
-      $input_icon_no = '';
-    }
+    echo "</tr>\n</table>";
+  }
+
+  //登録ボタン出力
+  private static function OutputSubmit() {
     echo <<<EOF
-</td>
-</tr>
+</td></tr>
 <tr>
 <td class="submit" colspan="3">
 <span class="explain">
@@ -420,24 +403,95 @@ EOF;
 </tr>
 </table>
 </td></tr>
+EOF;
+  }
+
+  //アイコン選択フォーム出力
+  private static function OutputIcon() {
+    $format = <<<EOF
 <tr><td>
-<fieldset><legend><img src="{$path}/icon.gif" alt="アイコン"></legend>
+<fieldset><legend><img src="%s/icon.gif" alt="アイコン"></legend>
 <table class="icon">
 <tr><td colspan="5">
-<input id="fix_number" type="radio" name="icon_no"{$icon_checked}><label for="fix_number">手入力</label>
-<input type="text" name="icon_no" size="10px" value="{$input_icon_no}">(半角英数で入力してください)
+<input id="fix_number" type="radio" name="icon_no"%s><label for="fix_number">手入力</label>
+<input type="text" name="icon_no" size="10px" value="%s">(半角英数で入力してください)
 </td></tr>
 <tr><td colspan="5">
 
 EOF;
+    if (isset(RQ::$get->icon_no) && RQ::$get->icon_no > (RQ::$get->user_no > 0 ? -1 : 0)) {
+      $checked = ' checked';
+      $icon_no = RQ::$get->icon_no;
+    } else {
+      $checked = '';
+      $icon_no = '';
+    }
+
+    printf($format, self::PATH, $checked, $icon_no);
     IconHTML::Output('user_manager');
     echo <<<EOF
 </tr></table>
 </fieldset>
 </td></tr>
-</table></div></form>
+EOF;
+  }
+
+  //ユーザ名フォーム生成
+  private static function GenerateUname() {
+    $format = <<<EOF
+<tr>
+<td class="img"><label for="uname"><img src="%s/uname.gif" alt="ユーザ名"></label></td>
 
 EOF;
-    HTML::OutputFooter();
+
+    if (RQ::$get->user_no > 0) { //登録情報変更モード時はユーザ名は変更不可
+      $format .= <<<EOF
+<td>%s</td>
+<td class="explain">%s</td>
+</tr>
+EOF;
+      $str = self::UNAME_EXPLAIN_HEADER . '<br>' . self::UNAME_EXPLAIN_FOOTER;
+      return sprintf($format, self::PATH, RQ::$get->uname, $str);
+    }
+    elseif (GameConfig::TRIP) { //トリップ対応
+      $format .= self::UNAME_INPUT . "\n" . <<<EOF
+<td><label for="trip">＃</label> <input type="text" id="trip" name="trip" size="15" maxlength="15" value="%s"></td>
+</tr>
+<tr>
+<td></td>
+<td colspan="2" class="explain">%s</td>
+</tr>
+EOF;
+      $str = self::UNAME_EXPLAIN_HEADER . self::UNAME_EXPLAIN_FOOTER . self::UNAME_TRIP;
+      if (DB::$ROOM->IsOption('necessary_name') && DB::$ROOM->IsOption('necessary_trip')) {
+	$str .= '<br><span>必ずユーザ名・トリップの両方を入力してください</span>';
+      } elseif (DB::$ROOM->IsOption('necessary_name')) {
+	$str .= '<br><span>必ずユーザ名を入力してください</span>';
+      } elseif (DB::$ROOM->IsOption('necessary_trip')) {
+	$str .= '<br><span>必ずトリップを入力してください</span>';
+      }
+      return sprintf($format, self::PATH, RQ::$get->uname, RQ::$get->trip, $str);
+    }
+    else {
+      $format .= self::UNAME_INPUT . "\n" . <<<EOF
+<td class="explain">%s(<span>トリップ使用不可</span>)</td>
+</tr>
+EOF;
+      $str = self::UNAME_EXPLAIN_HEADER . '<br>' . self::UNAME_EXPLAIN_FOOTER;
+      return sprintf($format, self::PATH, RQ::$get->uname, $str);
+    }
+  }
+
+  //パスワードフォーム生成
+  private static function GeneratePassword() {
+    if (RQ::$get->user_no > 0) return '';
+    $format = <<<EOF
+<tr>
+<td class="img"><label for="password"><img src="%s/password.gif" alt="パスワード"></label></td>
+<td><input type="password" id="password" name="password" size="30" maxlength="30" value=""></td>
+<td class="explain">セッションが切れた場合のログイン時に使います<br> (<span>暗号化されていないので要注意</span>)</td>
+</tr>
+EOF;
+    return sprintf($format, self::PATH);
   }
 }
