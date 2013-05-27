e0b1c7dd66b3523695a80d3bf01164575a657497
umethyl <umethyl@a7a435a0-a25e-405b-8339-bcd30a48c3eb>
2013-05-27 15:35:59 +0000

    JinroTwitter : 最適化
    git-svn-id: http://svn.sourceforge.jp/svnroot/jinrousiki/trunk@822 a7a435a0-a25e-405b-8339-bcd30a48c3eb

--- include/twitter_class.php
+++ include/twitter_class.php
@@ -1,19 +1,20 @@
 <?php
 //-- Twitter 投稿クラス --//
 class JinroTwitter {
-  const LIMIT = 140; //制限文字数
-  const URL   = 'https://api.twitter.com/1/statuses/update.json'; //送信先 URL
-  const SHORT = 'http://tinyurl.com/api-create.php?url='; //短縮 URL
+  const LIMIT  = 140; //制限文字数
+  const ENCODE = 'UTF-8'; //文字コード
+  const URL    = 'https://api.twitter.com/1/statuses/update.json'; //送信先 URL
+  const SHORT  = 'http://tinyurl.com/api-create.php?url='; //短縮 URL
 
   //投稿処理
   static function Send($id, $name, $comment) {
     if (TwitterConfig::DISABLE || ServerConfig::SECRET_ROOM) return true;
 
     $str = TwitterConfig::GenerateMessage($id, $name, $comment);
-    if (ServerConfig::ENCODE != 'UTF-8') { //Twitter は UTF-8
-      $str = mb_convert_encoding($str, 'UTF-8', ServerConfig::ENCODE);
+    if (ServerConfig::ENCODE != self::ENCODE) {
+      $str = mb_convert_encoding($str, self::ENCODE, ServerConfig::ENCODE);
     }
-    if (mb_strlen($str) > self::LIMIT) $str = mb_substr($str, 0, self::LIMIT - 1);
+    if (self::OverLimit($str)) $str = mb_substr($str, 0, self::LIMIT - 1);
 
     if (TwitterConfig::ADD_URL) {
       $url = ServerConfig::SITE_ROOT;
@@ -22,10 +23,9 @@ class JinroTwitter {
 	$short_url = @file_get_contents(self::SHORT . $url);
 	if ($short_url != '') $url = $short_url;
       }
-      if (mb_strlen($str . $url) + 1 < self::LIMIT) $str .= ' ' . $url;
+      if (! self::OverLimit($str . $url, 1)) $str .= ' ' . $url;
     }
-    if (0 < strlen(TwitterConfig::HASH) &&
-	mb_strlen($str . TwitterConfig::HASH) + 2 < self::LIMIT) {
+    if (0 < strlen(TwitterConfig::HASH) && ! self::OverLimit($str . TwitterConfig::HASH, 2)) {
       $str .= sprintf(' #%s', TwitterConfig::HASH);
     }
 
@@ -36,8 +36,13 @@ class JinroTwitter {
 
     if (! ($response === false || strrpos($response, 'error'))) return true;
     //エラー処理
-    $error = 'Twitter への投稿に失敗しました。<br>'."\n" . 'メッセージ：' . $str;
-    Text::p($error);
+    Text::Output('Twitter への投稿に失敗しました。', true);
+    Text::p($str, 'メッセージ');
     return false;
+  }
+
+  //文字数制限チェック
+  private static function OverLimit($str, $add = 0) {
+    return mb_strlen($str) + $add > self::LIMIT;
   }
 }
