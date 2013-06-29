7bdfc032458e760da3687f47a39c264c7478cc43
umethyl <umethyl@a7a435a0-a25e-405b-8339-bcd30a48c3eb>
2013-06-29 13:46:58 +0000

    README : ライセンス情報を追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/jinrousiki/trunk@859 a7a435a0-a25e-405b-8339-bcd30a48c3eb

--- info/top.php
+++ info/top.php
@@ -7,7 +7,7 @@ This server is Japanese only. -&gt; <a href="http://sourceforge.jp/projects/jinr
 </font>
 
 <h1>TOPIC</h1>
-<h2>Ver. 2.2.0β2アップロード (2013/06/23 (Sun) 17:25) → <a href="http://sourceforge.jp/projects/jinrousiki/releases/59041/">ダウンロード</a> (SourceForge)</h2>
+<h2>Ver. 2.2.0 RC1 アップロード (2013/06/23 (Sun) 17:25) → <a href="http://sourceforge.jp/projects/jinrousiki/releases/59041/">ダウンロード</a> (SourceForge)</h2>
 <ul>
   <!-- <li>→ <a href="info/develop/debug.php#ver150b15">デバッグ情報</a> (2011/11/06 (Sun) 04:20)</li> -->
 <li>データキャッシュ機能：ゲーム内：アイコンの表示設定に対応</li>
@@ -18,7 +18,7 @@ This server is Japanese only. -&gt; <a href="http://sourceforge.jp/projects/jinr
 <h1>開発状況</h1>
 <h2>新規実装 / 仕様変更</h2>
 <ul>
-<li>なし</li>
+<li>Twitter API 1.1 に対応</li>
 </ul>
 
 <h2>現在作成中 / 公開テスト待ち</h2>
